24d038dd4b16cb4bbf00bbfdeaa314d6491a5fcd
duck <al.wenn05@gmail.com>
2025-06-08 01:38:55 -0700

    more accuracy checks, more inputs
    - multiple accuracy checks, perfect, good, bad, miss, added rectangles to visualize.
    
    - takes L R U D - inputs instead of single input
    
    - the scene can now be called with a string input consisting of L R U D -, which can determine how the level will play out. The code will decode the string and push it to the notes array.
    
    - the 'beats' now delete when finished

--- src/Scenes/game.js
+++ src/Scenes/game.js
@@ -1,6 +1,8 @@
 class Game extends Phaser.Scene {
     constructor() {
         super({ key: 'Game' });
+
+        this.chart = (config && config.chart) || "L - R - U - D";
     }
 
     init() {
@@ -9,8 +11,19 @@ class Game extends Phaser.Scene {
 
     create() {
         this.lineY = this.game.config.height / 2;
-
         this.hitZoneX = 100;
+        this.noteSpeed = 200;
+
+        this.perfectZone = 20;
+        this.goodZone = 60;
+        this.badZone = 120;
+
+        this.directions = [
+            { key: 'LEFT',  char: 'L', color: 0xff4444 },
+            { key: 'DOWN',  char: 'D', color: 0x44ff44 },
+            { key: 'UP',    char: 'U', color: 0x4444ff },
+            { key: 'RIGHT', char: 'R', color: 0xffff44 }
+        ];
 
         this.line = this.add.line(
             0, 0,
@@ -19,17 +32,38 @@ class Game extends Phaser.Scene {
             0xffffff
         ).setOrigin(0, 0);
 
-        this.hitZone = this.add.rectangle(
-            this.hitZoneX, this.lineY, 20, 600, 0xffffff, 0
-        ).setStrokeStyle(2, 0xffffff);
+        this.add.rectangle(
+            this.hitZoneX, this.lineY, this.badZone, 480, 0xffaaaa, 0
+        ).setStrokeStyle(2, 0xffaaaa);
+        this.add.rectangle(
+            this.hitZoneX, this.lineY, this.goodZone, 480, 0xffffaa, 0
+        ).setStrokeStyle(2, 0xffffaa);
+        this.add.rectangle(
+            this.hitZoneX, this.lineY, this.perfectZone, 480, 0xaaffaa, 0
+        ).setStrokeStyle(2, 0xaaffaa);
 
-        this.noteSpeed = 200;
-        this.noteActive = true;
-        this.note = this.add.circle(
-            this.game.config.width - 50, this.lineY, 15, 0xffffff
-        );
 
-        this.input.keyboard.on('keydown-SPACE', this.handleInput, this);
+        this.notes = [];
+        let chartArr = this.chart.split(/\s+/);
+        let spacing = 80;
+        let startX = this.game.config.width - 50;
+        chartArr.forEach((char, i) => {
+            if (char === '-' || char === '') {
+                return;
+            }
+            let dir = this.directions.find(d => d.char === char.toUpperCase());
+            if (dir) {
+                this.notes.push({
+                    x: startX + i * spacing,
+                    y: this.lineY,
+                    dir: dir,
+                    active: true,
+                    circle: null
+                });
+            }
+        });
+
+        this.input.keyboard.on('keydown', this.handleInput, this);
 
         this.feedback = this.add.text(
             this.game.config.width / 2, this.lineY + 60, '',
@@ -37,26 +71,61 @@ class Game extends Phaser.Scene {
         ).setOrigin(0.5);
     }
 
-    handleInput() {
-        if (!this.noteActive) return;
+    handleInput(event) {
+        let key = event.key.replace('Arrow', '').toUpperCase();
+        //console.log(event.code + " " + key);
+        let dir = this.directions.find(d => d.key === key);
+        if (!dir) {
+            return;
+        }
 
-        if (Math.abs(this.note.x - this.hitZoneX) < 20) {
-            this.feedback.setText('good');
-            this.noteActive = false;
-            this.note.setFillStyle(0x888888);
-        } else {
+        let hit = false;
+        for (let i = 0; i < this.notes.length; i++) {
+            let note = this.notes[i];
+            if (note.active && note.dir.key === key) {
+                let dist = Math.abs(note.x - this.hitZoneX);
+                if (dist <= this.badZone / 2) {
+                    note.active = false;
+                    if (note.circle) {
+                        note.circle.destroy();
+                    }
+
+                    this.notes.splice(i, 1);
+
+                    if (dist <= this.perfectZone / 2) {
+                        this.feedback.setText('perfect');
+                    } else if (dist <= this.goodZone / 2) {
+                        this.feedback.setText('good');
+                    } else {
+                        this.feedback.setText('bad');
+                    }
+                    hit = true;
+                    break;
+                }
+            }
+        }
+        if (!hit) {
             this.feedback.setText('miss');
         }
     }
 
     update(time, delta) {
-        if (this.noteActive) {
-            this.note.x -= this.noteSpeed * (delta / 1000);
-
-            if (this.note.x < this.hitZoneX - 20) {
-                this.noteActive = false;
-                this.feedback.setText('miss');
-                this.note.setFillStyle(0x888888);
+        let dt = this.noteSpeed * (delta / 1000);
+        for (let i = this.notes.length - 1; i >= 0; i--) {
+            let note = this.notes[i];
+            if (note.active) {
+                note.x -= dt;
+                if (!note.circle) {
+                    note.circle = this.add.circle(note.x, note.y, 15, note.dir.color);
+                } else {
+                    note.circle.x = note.x;
+                }
+                if (note.x < 0) {
+                    note.active = false;
+                    if (note.circle) note.circle.destroy();
+                    this.notes.splice(i, 1);
+                    this.feedback.setText('miss');
+                }
             }
         }
     }
